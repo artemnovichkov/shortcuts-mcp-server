b3dbe6ee7a52ae7cd27c3f7124bce3f4675ec11c
Artem Novichkov <mail@artemnovichkov.com>
2025-05-12 17:54:32 +0500

    Add prompts

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,29 +10,35 @@ let runTool = Tool(name: "run",
                         "name": .object([
                             "description": "The name of the shortcut to run.",
                             "type": "string"
-                        ])
+                        ]),
                     ],
                     "required:": .array(["name"])
                    ]))
 
 let listTool = Tool(name: "list",
-                description: "List your shortcuts.",
-                inputSchema: .object([
-                    "type": "object",
-                ]))
+                    description: "List your shortcuts.",
+                    inputSchema: .object([
+                        "type": "object",
+                        "properties": [
+                            "show-identifiers": .object([
+                                "description": "Whether to show the identifiers of the shortcuts.",
+                                "type": "boolean"
+                            ])
+                        ],
+                    ]))
 
 let viewTool = Tool(name: "view",
-                   description: " View a shortcut in Shortcuts.",
-                   inputSchema: .object([
-                    "type": "object",
-                    "properties": [
-                        "name": .object([
-                            "description": "The name of the shortcut to run.",
-                            "type": "string"
-                        ])
-                    ],
-                    "required:": .array(["name"])
-                   ]))
+                    description: "View a shortcut in Shortcuts.",
+                    inputSchema: .object([
+                        "type": "object",
+                        "properties": [
+                            "name": .object([
+                                "description": "The name of the shortcut to view.",
+                                "type": "string"
+                            ])
+                        ],
+                        "required:": .array(["name"])
+                    ]))
 
 let tools = [runTool, listTool, viewTool]
 
@@ -49,7 +55,7 @@ try await server.start(transport: transport)
 
 let shortcutsURI = "shortcuts://list"
 
-await server.withMethodHandler(ListResources.self) { params in
+await server.withMethodHandler(ListResources.self) { _ in
     ListResources.Result(resources: [.init(name: "Shortcuts", uri: shortcutsURI, mimeType: "text/plain")])
 }
 
@@ -84,22 +90,108 @@ func call(tool: Tool, params: CallTool.Parameters) async throws -> String {
     let arguments: Arguments
     switch tool.name {
     case "run":
-        guard let name = params.arguments?["name"]?.stringValue else {
-            throw MCPError.invalidParams("Missing name argument")
-        }
+        let name = try getParam(name: "name", params: params)
         arguments = ["run", name]
     case "list":
-        arguments = ["list"]
-    case "view":
-        guard let name = params.arguments?["name"]?.stringValue else {
-            throw MCPError.invalidParams("Missing name argument")
+        let showIdentifiers = params.arguments?["show-identifiers"]?.boolValue ?? false
+        if showIdentifiers {
+            arguments = ["list", "--show-identifiers"]
+        } else {
+            arguments = ["list"]
         }
+    case "view":
+        let name = try getParam(name: "name", params: params)
         arguments = ["view",  name]
     default:
         throw MCPError.invalidParams("Unknown tool name: \(tool.name)")
     }
     let result = try await run(executable, arguments: arguments)
     return result.standardOutput ?? ""
+}
+
+func getParam(name: String, params: CallTool.Parameters) throws -> String {
+    guard let value = params.arguments?[name]?.stringValue else {
+        throw MCPError.invalidParams("Missing parameter: \(name)")
+    }
+    return value
+}
+
+// MARK: - Prompts
+
+struct ShortcutPrompt {
+
+    enum PromptType: String {
+        case run
+        case list
+        case view
+
+        var description: String {
+            switch self {
+            case .run: return "Run a shortcut."
+            case .list: return "List your shortcuts."
+            case .view: return "View a shortcut in Shortcuts."
+            }
+        }
+    }
+
+    let type: PromptType
+
+    func prompt(with arguments: [Prompt.Argument]) -> Prompt {
+        Prompt(name: type.rawValue, description: type.description, arguments: arguments)
+    }
+
+    func result(for params: GetPrompt.Parameters, messages: (GetPrompt.Parameters) throws -> ([Prompt.Message])) rethrows -> GetPrompt.Result {
+        GetPrompt.Result(description: type.description, messages: try messages(params))
+    }
+}
+
+let runPrompt = ShortcutPrompt(type: .run)
+let listPrompt = ShortcutPrompt(type: .list)
+let viewPrompt = ShortcutPrompt(type: .view)
+
+await server.withMethodHandler(ListPrompts.self) { params in
+    let nameArgument = Prompt.Argument(name: "name",
+                                       description: "The name of the shortcut to run.",
+                                       required: true)
+    let showIdentifiersArgument = Prompt.Argument(name: "show-identifiers",
+                                                  description: "Whether to show the identifiers of the shortcuts.")
+    return ListPrompts.Result(prompts: [runPrompt.prompt(with: [nameArgument]),
+                                        listPrompt.prompt(with: [showIdentifiersArgument]),
+                                        viewPrompt.prompt(with: [nameArgument])])
+}
+
+await server.withMethodHandler(GetPrompt.self) { params in
+    guard let type = ShortcutPrompt.PromptType(rawValue: params.name) else {
+        throw MCPError.invalidParams("Unknown prompt name: \(params.name)")
+    }
+    return switch type {
+    case .run:
+        try runPrompt.result(for: params) { params in
+            let name = try getParam(name: "name", params: params)
+            return [.init(role: .user, content: .text(text: "Run \(name) shortcut"))]
+        }
+    case .list:
+        listPrompt.result(for: params) { params in
+            var text = "List shortcuts"
+            if params.arguments?["show-identifiers"]?.boolValue == true {
+                text += ", show identifiers"
+            }
+            return [.init(role: .user, content: .text(text: text))]
+        }
+    case .view:
+        try viewPrompt.result(for: params) { params in
+            let name = try getParam(name: "name", params: params)
+            return [.init(role: .user, content: .text(text: "View \(name) shortcut"))]
+        }
+
+    }
+}
+
+func getParam(name: String, params: GetPrompt.Parameters) throws -> String {
+    guard let value = params.arguments?[name]?.stringValue else {
+        throw MCPError.invalidParams("Missing parameter: \(name)")
+    }
+    return value
 }
 
 // MARK: - Run
