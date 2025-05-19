0ea12770f28ebec472965c86e898cc73e93a1ccf
Artem Novichkov <mail@artemnovichkov.com>
2025-05-19 20:05:53 +0500

    Update paths

--- mise/tasks/release.sh
+++ mise/tasks/release.sh
@@ -10,11 +10,8 @@ swift build -c release --triple arm64-apple-macosx
 
 temp_dir=$(mktemp -d)
 zip_path="$temp_dir/$usage_product-macos.zip"
-/usr/bin/ditto -c -k --keepParent .build/"$usage_product" "$zip_path"
+/usr/bin/ditto -c -k --keepParent .build/release/"$usage_product" "$zip_path"
 trap 'rm -rf "$temp_dir"' EXIT
 
-gh release create "$usage_version" 
-  --title "$usage_version" 
-  --notes "Release $usage_version" 
-  "$zip_path"
+gh release create "$usage_version" --title "$usage_version"  --notes "Release $usage_version" "$zip_path"
 echo "Release $usage_version created and uploaded successfully"
